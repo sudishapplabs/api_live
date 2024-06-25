a687a89b2d1191f348771b3e69fe1c4bf5227771
sudishapplabs <sudish@applabs.ai>
2024-06-25 20:15:00 +0530

    add 500mb

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-app.use(express.json({ limit: '50mb'}));
+app.use(express.json({ limit: '500mb' }));
 //app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.urlencoded({ limit: '500mb', extended: true, parameterLimit: 500000 }));
 connectMongoDatabase();
 
 const cors = require("cors");
