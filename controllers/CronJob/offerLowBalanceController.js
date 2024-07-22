255cf67c06b7da844f42a26dd0b8e4c2ca4a4ffa
sudishapplabs <sudish@applabs.ai>
2024-07-22 08:56:31 +0530

    by sudish

--- controllers/CronJob/offerLowBalanceController.js
+++ controllers/CronJob/offerLowBalanceController.js
@@ -13,7 +13,6 @@ const { isArray } = require("util");
 
 
 exports.getOffersLowBalance = async (req, res) => {
-  process.exit();
   const offerData = await getAllOffersByStatus();
 
   if (Array.isArray(offerData) && offerData.length > 0) {
